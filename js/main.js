92b81f52317e5431d9a6aceb5e68548bf38e87ed
tecnibus5 <tecnibus5@us.es>
2023-11-14 13:17:59 +0100

    feat: Añade formato visual

--- js/main.js
+++ js/main.js
@@ -83,45 +83,83 @@ async function load_game() {
     inicioText.insertAdjacentHTML('afterbegin',`<h1 class="cover-text cover-title">${cover['titulo']}</h1>`);
     inicioDiv.insertAdjacentHTML('afterbegin',`<div class="div-cover-img"><img src="${cover['img']}" alt="Portada" class="cover-img"></div>`);
 
+    let res = Array.of(cover,scenes);
+
+    return res;
 }
 
-document.addEventListener('DOMContentLoaded',load_game)
+document.addEventListener('DOMContentLoaded', async () => {
+    let dataList = await load_game();
+    let startButton = document.getElementById('btn-start-game');
+    startButton.addEventListener('click',() => main(dataList));
+})
 
-async function main() {
-    document.getElementById('boton-bonito-wrapper').remove();
+function generate_action_html(action,sceneId) {
+    return `<span id="${sceneId}-${action}" data-action="${action}" class="action-text">${action}</span>`;
+}
 
-    let gameInput = document.getElementById('game-input');
+function generate_action_input(sceneId) {
+    return `<form action="" id="text-input-form"><input id="text-input-${sceneId}" class="game-text-input" type="text"><button type="submit" class="submit-text-input">Enviar</button></form>`;
+}
+
+async function main(dataList) {
+    let scenes = dataList[1];
+    document.getElementById('inicio-juego').remove();
+
+    let inputArea = document.getElementById('action-selectors');
     let gameScreen = document.getElementById('game');
+    let textScreen = document.getElementById('scene-text');
+    gameScreen.classList.remove('d-none');
     let currentScene = load_initial_scene(scenes);
 
-    gameInput.addEventListener('keyup', function(e) {
-        if(e.key === 'Enter') {
-            handle_user_input(e.target.value);
-            e.target.value = '';
-        }
-    })
-
-    render_text(currentScene)
-
+    render_text(currentScene);
+    regenerate_actions();
+    
     function render_text(scene) {
+        textScreen.innerHTML = '';
         for(line of scene.textos) {
-            gameScreen.insertAdjacentHTML('beforeend',`<div class="text-style game-text">${texts[line-1]}</div>`);
+            textScreen.insertAdjacentHTML('beforeend',`<p class="text-style game-text">${line}</p>`);
         }
     }
 
-    function handle_user_input(input) {
-        let action = input.trim().toUpperCase();
+    function handle_user_input(event) {
+        let action;
+        if (currentScene.tipo === "ACCIÓN" || currentScene.tipo === "INICIAL") {
+            action = event.target.dataset.action;
+        } else if (currentScene.tipo === "TEXTO") {
+            action = event.trim().toUpperCase();
+        }
         if (check_valid_action(action,currentScene)) {
             let nextScene = get_next_scene(currentScene,action);
             currentScene = scenes.find(s => s.id === nextScene);
-            gameScreen.innerHTML = '';
-            gameScreen.scrollTo(0,0);
-            render_text(currentScene,texts);
-            if (currentScene.tipo === "FINAL") {
-                gameInput.disabled = true;
-            }
+            render_text(currentScene);
+            regenerate_actions();
         } else {
-            gameScreen.insertAdjacentHTML('beforeend',`<span class="text-style">${currentScene.error}</span>`)
+            let error = document.getElementById('id-error-text');
+            if (!error) {
+                let inputForm = document.getElementById('text-input-form');
+                inputForm.insertAdjacentHTML('beforeend',`<span id="id-error-text" class="form-error">${currentScene.error}</span>`);
+            }
+        }
+    }
+
+    function regenerate_actions() {
+        // Si la escena es de botones
+        inputArea.innerHTML = '';
+        if (currentScene.tipo === "ACCIÓN" || currentScene.tipo === "INICIAL") {
+            for (let action of Object.keys(currentScene.acciones)) {
+                inputArea.insertAdjacentHTML('beforeend',generate_action_html(action,currentScene.id));
+                let actionInput = document.getElementById(`${currentScene.id}-${action}`);
+                actionInput.addEventListener('click',(event) => handle_user_input(event));
+            }
+        } else if (currentScene.tipo === "TEXTO") {
+            inputArea.insertAdjacentHTML('beforeend', generate_action_input(currentScene.id));
+            let textInputForm = document.getElementById('text-input-form');
+            textInputForm.addEventListener('submit',function(e) {
+                e.preventDefault();
+                let innerInput = document.getElementById(`text-input-${currentScene.id}`);
+                handle_user_input(innerInput.value);
+            })
         }
     }
 
